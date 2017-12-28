a2023e5a6b858113470869fd11d9278b8b3ebd0c
Andrew Widdersheim <amwiddersheim@gmail.com>
2017-12-28 12:00:07 -0500

    Use log_write() in log_fatal()

--- log.h
+++ log.h
@@ -8,7 +8,7 @@ void log_write(const char *message, ...) __attribute__((format (printf, 1, 2)));
 #define log_info(M, ...) log_write("[INFO]: " M "\n", ##__VA_ARGS__);
 #define log_error(M, ...) log_write("[ERROR]: " M "\n", ##__VA_ARGS__);
 #define log_fatal(M, ...) do {\
-    fprintf(stdout, "[FATAL]: " M "\n", ##__VA_ARGS__); \
+    log_write("[FATAL]: " M "\n", ##__VA_ARGS__); \
     exit(1); \
 } while(0)
 #define log_errno(M, ...) log_fatal(M " errno (%d) msg (%s)\n", ##__VA_ARGS__, errno, strerror(errno));
